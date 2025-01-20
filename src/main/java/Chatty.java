c33316bd552c2d276ac58d66eb05b624eb8d7caf
Siyan-G <gsy7418@gmail.com>
2025-01-20 10:04:52 +0800

    rename file name, add intro and exit messages

---
+++ src/main/java/Chatty.java
@@ -0,0 +1,12 @@
+public class Chatty {
+    public static void main(String[] args) {
+        String linebreak = "____________________________________________________________";
+        String introMsg = "Hello! I'm Chatty\nWhat can I do for you?";
+        String exitMsg = "Bye. Hope to see you again soon!";
+        System.out.println(linebreak);
+        System.out.println(introMsg);
+        System.out.println(linebreak);
+        System.out.println(exitMsg);
+        System.out.println(linebreak);
+    }
+}
