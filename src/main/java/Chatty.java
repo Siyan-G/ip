0587737353482ec744707f6da0387679fe99a756
Siyan-G <gsy7418@gmail.com>
2025-01-22 21:01:18 +0800

    Add, List

--- src/main/java/Chatty.java
+++ src/main/java/Chatty.java
@@ -10,11 +10,12 @@ public class Chatty {
         System.out.println(linebreak);
 
         Scanner scanner = new Scanner(System.in);
+        String[] taskList = new String[100];
+        int taskCounter = 0;
 
         while (true) {
             System.out.print("Master: ");
             String userInput = scanner.nextLine();
-
 
             if (userInput.equalsIgnoreCase("bye")) {
                 System.out.println(exitMsg);
@@ -22,7 +23,26 @@ public class Chatty {
                 break;
             }
 
-            System.out.println("Chatty: " + userInput);
+            if (userInput.equalsIgnoreCase("list")) {
+                if (taskCounter == 0) {
+                    System.out.println("Chatty: your task list is empty.");
+                }
+                else {
+                    System.out.printf("Chatty: your have %d tasks in progress:\n", taskCounter);
+                    for (int i = 0; i < taskList.length; i++) {
+                        if (taskList[i] == null) {
+                            break;
+                        }
+                        System.out.printf("%d. %s\n", i + 1, taskList[i]);
+                    }
+                    System.out.println(linebreak);
+                }
+                continue;
+            }
+
+            taskList[taskCounter] = userInput;
+            taskCounter++;
+            System.out.println("Chatty: I have added task: " + userInput);
             System.out.println(linebreak);
         }
     }
