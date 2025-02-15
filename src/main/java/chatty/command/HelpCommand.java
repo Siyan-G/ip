9a5d755edced30ac896b34d1541d1054e5a68697
Siyan-G <gsy7418@gmail.com>
2025-02-15 09:34:26 +0800

    add javadoc in HelpCommand class

--- src/main/java/chatty/command/HelpCommand.java
+++ src/main/java/chatty/command/HelpCommand.java
@@ -5,17 +5,22 @@ import chatty.exception.ChattyTaskNotFoundException;
 import chatty.task.TaskList;
 import chatty.ui.Ui;
 
+/**
+ * Represents a command that provides help information to the user.
+ */
 public class HelpCommand extends Command {
 
     /**
-     * @param tasks   The task list to be modified.
+     * Executes the HelpCommand to display a list of available commands to the user.
+     *
+     * @param tasks   The task list (not used in this command but required by the method signature).
      * @param ui      The user interface to interact with the user.
-     * @param storage The storage to save or load data.
-     * @return
-     * @throws ChattyTaskNotFoundException
+     * @param storage The storage system (not used in this command but required by the method signature).
+     * @return A string containing the help message.
+     * @throws ChattyTaskNotFoundException If an error occurs while executing the command.
      */
     @Override
     public String execute(TaskList tasks, Ui ui, Storage storage) throws ChattyTaskNotFoundException {
-        return "";
+        return ui.sendHelp();
     }
 }
